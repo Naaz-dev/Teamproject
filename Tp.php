62bda0dcf66e049027627158f6a5d6fbe02974f3
DaBluePotato <167704263+DaBluePotato@users.noreply.github.com>
2025-09-11 12:30:50 +0200

    Create Tp.php

---
+++ Tp.php
@@ -0,0 +1,50 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>naam</th>
+            <th>klas</th>
+            <th>Tickets</th>
+        </tr>
+        <?php
+        $servername = "mySql";
+        $username = "root";
+        $password = "password";
+        $dbname = "Teamproject";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT naam, ID, klas, tickets FROM Student";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['ID'] . "</a></td>";
+                echo "<td><a href='ShowTickets.php?name=" . $row['naam'] . "'>" . $row['naam'] . "</a></td>";
+                echo "<td>" . $row['klas'] . "</td>";
+                echo "<td>" . $row['tickets'] . "</td>";
+                echo "</tr>";
+            }
+
+
+        }
+
+        $conn->close();
+        ?>
+
+</body>
+
+</html>
